f61394571c39feaa84f57d8ffbfb8293370638e2
gdgf <794428738@qq.com>
2020-02-15 22:21:29 +0800

    add 4优化

---
+++ 4优化/practice4.5/opt4_8_forward.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+# 0导入模块 ，生成模拟数据集
+import tensorflow as tf
+
+
+# 定义神经网络的输入、参数和输出，定义前向传播过程
+# 权重/参数
+def get_weight(shape, regularizer):
+    w = tf.Variable(tf.random_normal(shape), dtype=tf.float32)
+    # 表示将参数 w 正则化损失加到总损失 losses 中。
+    tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(regularizer)(w))
+    return w
+# 输出
+def get_bias(shape):
+    b = tf.Variable(tf.constant(0.01, shape=shape))
+    return b
+    
+# 前向传播过程
+def forward(x, regularizer):
+    w1 = get_weight([2, 11], regularizer)
+    b1 = get_bias([11])
+    y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+
+    w2 = get_weight([11, 1], regularizer)
+    b2 = get_bias([1])
+    y = tf.matmul(y1, w2) + b2
+    return y
